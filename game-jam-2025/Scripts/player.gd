8b87c5ae9d2d75bf8d94afdf9666224f095e33e8
Braxton Z Ogletree <bzogletree@gmail.com>
2025-01-18 20:49:36 -0600

    The basic prototype baseline for MVP to test idea

---
+++ game-jam-2025/Scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+const speed = 250
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	get_input()
+	move_and_slide()
+	pass
+
+func get_input():
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = input_direction * speed
---
+++ game-jam-2025/Scripts/target_script.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+enum {
+	IDLE,
+	WANDER,
+	WORKING,
+	SUSPICIOUS,
+	ALERT,
+	PANIC,
+	ATTACKING
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
